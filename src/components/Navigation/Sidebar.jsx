f39e97020696f6599162b73870da1fd7f8cd945c
bfeelin <brianphelan59@gmail.com>
2023-08-15 23:19:50 -0400

    add sidebar for app routing interface

---
+++ src/components/Navigation/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+    Flex,
+    Divider,
+    useColorModeValue,
+} from '@chakra-ui/react'
+import {
+    FiHome,
+} from 'react-icons/fi'
+import NavItem from './NavItem'
+
+export default function Sidebar() {
+    return (
+        <Flex
+            h="91vh"
+            display={{base: 'none', md: 'flex'}}
+            boxShadow="0 12px 12px 0 rgba(0, 0, 0, 0.05)"
+            w={"200px"}
+            backgroundColor={useColorModeValue('white', 'midnight')}
+            flexDir="column"
+            justifyContent="flex-start"
+            >
+           <>
+            <Flex
+                flexDir="column"
+                width={'100%'}
+                alignItems={"flex-start"}
+                as="nav"
+            >
+                <NavItem icon={FiHome} name="Home" to="/"/>
+                <Divider/>
+
+            </Flex>
+            </>
+            
+        </Flex>
+    
+    )
+}
