769b9a71240f597d8d67fe69b36078f65fab3b30
bfeelin <brianphelan59@gmail.com>
2023-08-15 23:03:05 -0400

    Add chakra theme provider, theme, breakpoints

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { ChakraProvider, ColorModeScript, extendTheme, useColorModeValue } from '@chakra-ui/react'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+
+const breakpoints = createBreakpoints({
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+})
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      green: "#019771",
+      blue: "#0047AB"
+    },
+  },
+
+  config,
+  breakpoints
+  }
+  )
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
