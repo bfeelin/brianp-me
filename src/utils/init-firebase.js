ffdf6373889011f91674c834f71f21cabf6759c8
bfeelin <brianphelan59@gmail.com>
2023-08-15 23:20:05 -0400

    Remove auth

--- src/utils/init-firebase.js
+++ src/utils/init-firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/compat/app"
 import { getFirestore } from "@firebase/firestore"
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -13,6 +14,6 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
-const auth = getAuth(app);
+// const auth = getAuth(app);
 
 export { app, db, firebase, auth };
