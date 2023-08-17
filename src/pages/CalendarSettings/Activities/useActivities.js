d054d5f26a121f555ee980c5e9d6f06b74619b6f
bfeelin <brianphelan59@gmail.com>
2023-08-17 15:50:21 -0400

    Add functions to mark activity deleted & restore

--- src/pages/CalendarSettings/Activities/useActivities.js
+++ src/pages/CalendarSettings/Activities/useActivities.js
@@ -49,11 +49,31 @@ const useActivities = () => {
     }
   };
 
+  const markActivityDeleted = async (activityId) => {
+    try {
+      const activityRef = doc(db, 'activities', activityId);
+      await updateDoc(activityRef, {deleted: true});
+    } catch (error) {
+      console.error('Error updating activity:', error);
+    }
+  };
+
+  const restoreActivity = async (activityId) => {
+    try {
+      const activityRef = doc(db, 'activities', activityId);
+      await updateDoc(activityRef, {deleted: false});
+    } catch (error) {
+      console.error('Error updating activity:', error);
+    }
+  };
+
   return {
     activities,
     createActivity,
     updateActivity,
     deleteActivity,
+    markActivityDeleted,
+    restoreActivity
   };
 };
 
