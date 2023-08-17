a4f065bd9fa15a87ac72a9c14c38b7adc19e1f92
bfeelin <brianphelan59@gmail.com>
2023-08-17 15:35:51 -0400

    display current activities

---
+++ src/pages/CalendarSettings/Activities/ActivityList.jsx
@@ -0,0 +1,26 @@
+// TopicList.js
+import React, { useState } from 'react';
+import useActivities from './useActivities';
+import Activity from './Activity'
+import { Box, Text } from '@chakra-ui/react';
+
+function ActivityList( { activities } ) {
+  return (
+    <>
+        <Box mt='3' mb='3'>
+            <Text m='3' as='em'>Manage Activities</Text>
+            {activities?.map((activity) => (
+                <Activity 
+                    key={activity.id}
+                    activity={activity} 
+                    
+                />
+
+            ))}
+        </Box>
+    </>
+  );
+}
+
+export default ActivityList;
+
