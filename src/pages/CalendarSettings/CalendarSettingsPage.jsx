2c329e046eb51e23d9785471288a17ca3ba64df1
bfeelin <brianphelan59@gmail.com>
2023-08-17 18:07:01 -0400

    Add Calendars tab

--- src/pages/CalendarSettings/CalendarSettingsPage.jsx
+++ src/pages/CalendarSettings/CalendarSettingsPage.jsx
@@ -1,6 +1,7 @@
 import { Text } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import ManageActivities from "./Activities/ManageActivities";
+import ManageCalendars from "./Calendars/ManageCalendars";
 
 export default function CalendarSettingsPage( { } ){
 
@@ -8,9 +9,13 @@ export default function CalendarSettingsPage( { } ){
         <>
             <Tabs variant='enclosed' mt='6'>
                 <TabList>
+                    <Tab>Calendars</Tab>
                     <Tab>Activities</Tab>
                 </TabList>
                 <TabPanels>
+                    <TabPanel>
+                        <ManageCalendars />
+                    </TabPanel>
                     <TabPanel>
                         <ManageActivities />
                     </TabPanel>
