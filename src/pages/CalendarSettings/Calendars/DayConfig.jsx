ac544da28e7c37c60a0ff61379f412187037124f
bfeelin <brianphelan59@gmail.com>
2023-08-17 20:07:52 -0400

    Create a sortable list for each day

---
+++ src/pages/CalendarSettings/Calendars/DayConfig.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { useCalendars } from "./useCalendars";
+import { ReactSortable } from "react-sortablejs";
+import ActivityCard from "./ActivityCard";
+
+export default function DayConfig( { day, calendar, allActivities } ){
+    const { updateCalendar } = useCalendars(calendar.id)
+    const [theseActivities, setTheseActivities] = useState(day.activities ? day.activities : [])
+
+    useEffect(async() => {
+        const handleUpdateCalendar = async () => {
+            await updateCalendar(calendar.id, {
+                [`${day.title}`]: theseActivities
+            })
+        }
+
+        await handleUpdateCalendar()
+
+    }, [theseActivities])
+
+    const handleDeleteActivity = (thisID) => {
+        let activityIndex = theseActivities.findIndex(activity => activity.id === thisID)
+        if(activityIndex > -1){
+            let newActivities = [...theseActivities]
+            newActivities.splice(activityIndex, 1)
+            setTheseActivities(newActivities)
+        }
+    }
+
+    // const handleSetTheseActivities = async (newActivities) => {
+    //     setTheseActivities(newActivities)
+    //     await handleUpdateCalendar(newActivities)
+    // }
+
+    return(
+        <>
+
+            <ReactSortable   
+                key={`react-sortable-${calendar.id}-${day.title}`}     
+                clone={item => ({ id: item.id, title: item.title, description: item.description })}
+                list={theseActivities} 
+                setList={setTheseActivities}
+                group={{ name: "activities", pull: "clone" }}
+                animation="200"
+                easing="ease-out"
+                >
+                {theseActivities?.map((activity, i) => (
+                    <>
+                        <ActivityCard 
+                            handleDeleteActivity={handleDeleteActivity} 
+                            key={`activity-card-${calendar.id}-${day.title}-${activity.id}-${i}`} 
+                            activity={activity} 
+                            i={i} 
+                            maxWidth='100%' 
+                            allActivities={allActivities}
+                            calendar={calendar}
+                            day={day}
+                            />
+                   </>
+                ))}
+            </ReactSortable>
+
+     </>
+    )
+}
